54738d1b004e644a7521c421798dcfa05f5182eb
GJ Tiquia <gjtiquia@gmail.com>
2026-09-15 13:54:39 +0800

    fix db validation not showing the error message

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,12 +10,15 @@ if (!dbFilename) {
 
 const dbExists = await Bun.file(dbFilename).exists();
 if (!dbExists) {
-    throw new Error(`${dbFilename} does not exist! hint: bun run db:push`);
+    throw new Error(`${dbFilename} does not exist! \nhint: bun run db:push`);
 }
 
-const dbValidateOutput = await Bun.$`bun run db:validate`.text();
-if (!dbValidateOutput.includes("No changes detected")) {
-    throw new Error("db schema is out of sync! hint: bun run db:push");
+const { stdout, stderr, exitCode } = await Bun.$`bun run db:validate`.nothrow().quiet();
+if (exitCode !== 0) {
+    throw new Error(`db:validate non-zero exit code ${exitCode}! \nhint: bun run db:push \nstdout: \n${stdout} \nstderr: \n${stderr}`);
+}
+if (!stdout.includes("No changes detected")) {
+    throw new Error("db schema is out of sync! \nhint: bun run db:push");
 }
 
 export const db = drizzle(dbFilename);
